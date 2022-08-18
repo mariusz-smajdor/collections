ebe17d20faa8ef84500d89644383420762fd6ea5
Mariusz <smajdormariusz98@gmail.com>
2022-08-18 19:54:28 +0200

    Constant storage key names

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,7 @@
 export const LIGHT_THEME = 'light';
 export const DARK_THEME = 'dark';
 
+export const STORAGE_KEY_THEME = 'theme';
+export const STORAGE_KEY_TOKEN = 'token';
+
 export const MOBILE_MENU_WIDTH = 575;
