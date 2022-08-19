90a74b432c92c959c3f77125221ce0b0e0ab6a18
Mariusz <smajdormariusz98@gmail.com>
2022-08-19 23:32:00 +0200

    Removed messages from jwt auth middleware

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -4,13 +4,13 @@ const verifyJWT = (req, res, next) => {
   const token = req.headers['x-access-token'];
 
   if (!token) {
-    res.status(401).send('You are not authenticated.');
+    res.status(401);
     return;
   }
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
     if (err) {
-      res.status(403).send('You have no premission to access this resource');
+      res.status(403);
     } else {
       req.id = user.id;
       next();
