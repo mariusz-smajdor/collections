4141b59c1a56cb75bf556a7bb9bb207bfdaa3f81
Mariusz <smajdormariusz98@gmail.com>
2022-08-22 18:29:38 +0200

    Wrapped forms with Formik component

--- client/src/pages/NewCollection/NewField/index.jsx
+++ client/src/pages/NewCollection/NewField/index.jsx
@@ -1,16 +1,20 @@
+import { Formik } from 'formik';
+
 import { Label, Form, Input, Button } from '../../../assets/UI/formEls';
 import { Title } from '../../../assets/UI/textFormatEls';
 
 function NewField() {
   return (
-    <Form as='div'>
-      <Title as='h3'>Add New Field</Title>
-      <Label>
-        <Input placeholder='Field name' />
-        <Input placeholder='Field type' />
-      </Label>
-      <Button>Add New Field</Button>
-    </Form>
+    <Formik>
+      <Form>
+        <Title as='h3'>Add New Field</Title>
+        <Label>
+          <Input placeholder='Field name' />
+          <Input placeholder='Field type' />
+        </Label>
+        <Button>Add New Field</Button>
+      </Form>
+    </Formik>
   );
 }
 
--- client/src/pages/NewCollection/index.jsx
+++ client/src/pages/NewCollection/index.jsx
@@ -1,3 +1,5 @@
+import { Formik } from 'formik';
+
 import NewField from './NewField';
 import Container from '../../components/layout/Container';
 import { TOPICS } from './topics';
@@ -7,30 +9,34 @@ import { Title } from '../../assets/UI/textFormatEls';
 function NewCollection() {
   return (
     <Container medium>
-      <Form>
-        <Title>Create Collection</Title>
-        <Label>
-          <Text>Name:</Text>
-          <Input />
-        </Label>
-        <Label>
-          <Text>Description:</Text>
-          <Input />
-        </Label>
-        <Label>
-          <Text>Topic:</Text>
-          <Input as='select'>
-            {TOPICS.map((topic, i) => (
-              <option key={i}>{topic}</option>
-            ))}
-          </Input>
-        </Label>
-        <Label>
-          <Text>Add image:</Text>
-          <Input type='file' accept='.jpg,.jpeg,.png' />
-        </Label>
-        <Button>Create</Button>
-      </Form>
+      <Formik>
+        {() => (
+          <Form>
+            <Title>Create Collection</Title>
+            <Label>
+              <Text>Name:</Text>
+              <Input />
+            </Label>
+            <Label>
+              <Text>Description:</Text>
+              <Input />
+            </Label>
+            <Label>
+              <Text>Topic:</Text>
+              <Input as='select'>
+                {TOPICS.map((topic, i) => (
+                  <option key={i}>{topic}</option>
+                ))}
+              </Input>
+            </Label>
+            <Label>
+              <Text>Add image:</Text>
+              <Input type='file' accept='.jpg,.jpeg,.png' />
+            </Label>
+            <Button>Create</Button>
+          </Form>
+        )}
+      </Formik>
       <NewField />
     </Container>
   );
