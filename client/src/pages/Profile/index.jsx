e217a1ce0cf60bb6aefbb8bf951d9b25997c5355
Mariusz <smajdormariusz98@gmail.com>
2022-08-22 18:20:53 +0200

    User authentication logic moved into custom hook

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,35 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 import Container from '../../components/layout/Container';
-import { getLocalStorageItem } from '../../shared/utils/getLocalStorageItem';
-import { localStorageKeys } from '../../shared/constants/localStorageKeys';
+import { useAuth } from '../../shared/hooks/useAuth';
 import { routes } from '../../shared/constants/routes';
 import { Button } from '../../assets/UI/formEls';
 import { Title } from '../../assets/UI/textFormatEls';
 import { Group } from './styled';
 
-const { TOKEN } = localStorageKeys;
 const { NEW_COLLECTION } = routes;
 
 function Profile() {
-  const [user, setUser] = useState('');
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:3001/myself', {
-        headers: {
-          authorization: getLocalStorageItem(TOKEN),
-        },
-      })
-      .then(res => {
-        setUser(res.data[0]);
-      })
-      .catch(err => {
-        err.response.status === 403 && localStorage.removeItem('token');
-      });
-  }, []);
+  const user = useAuth();
 
   return (
     <Container medium>
