427332f02429937ae91dd0c3ac99868c18db6ac1
Mariusz <smajdormariusz98@gmail.com>
2022-08-23 19:36:02 +0200

    Styled for Formik Fields as select element

---
+++ client/src/pages/NewCollection/fieldStyles.js
@@ -0,0 +1,19 @@
+export const lightStyles = {
+  padding: '10px 20px',
+  width: '100%',
+  border: '1px solid #212121',
+  borderRadius: '5px',
+  background: '#fefefe',
+  color: '#141414',
+  outline: 'unset',
+};
+
+export const darkStyles = {
+  padding: '10px 20px',
+  width: '100%',
+  border: '1px solid #c5c5c5',
+  borderRadius: '5px',
+  background: '#282828',
+  color: '#fefefe',
+  outline: 'unset',
+};
