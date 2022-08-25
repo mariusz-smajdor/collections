8b383648cf30414ced87b4e2214ae03d799e52da
Mariusz <smajdormariusz98@gmail.com>
2022-08-25 12:34:18 +0200

    Collections display in profile

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
+import { RiDeleteBinLine, RiSettings3Line } from 'react-icons/ri';
 
 import Container from '../../components/layout/Container';
 import { useGetCollection } from '../../shared/hooks/useGetCollection';
 import { routes } from '../../shared/constants/routes';
 import { Button } from '../../assets/UI/formEls';
 import { Title } from '../../assets/UI/textFormatEls';
-import { Collection, Collections, Group, Text } from './styled';
+import {
+  Collection,
+  Collections,
+  Group,
+  Fields,
+  Field,
+  FieldTitle,
+  Settings,
+  Icon,
+} from './styled';
 
 const { NEW_COLLECTION } = routes;
 
@@ -23,6 +33,30 @@ function Profile() {
           Add New
         </Button>
       </Group>
+      {collections?.map(({ name, description, topic }, i) => (
+        <Collections key={i}>
+          <Collection>
+            <Fields>
+              <Field>
+                <FieldTitle>Name: </FieldTitle>
+                {name}
+              </Field>
+              <Field>
+                <FieldTitle>Topic: </FieldTitle>
+                {topic}
+              </Field>
+              <Field>
+                <FieldTitle>Description: </FieldTitle>
+                {description}
+              </Field>
+            </Fields>
+            <Settings>
+              <Icon as={RiSettings3Line} settings />
+              <Icon as={RiDeleteBinLine} />
+            </Settings>
+          </Collection>
+        </Collections>
+      ))}
     </Container>
   );
 }
--- client/src/pages/Profile/styled.js
+++ client/src/pages/Profile/styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Group = styled.div`
   display: flex;
@@ -11,5 +11,76 @@ export const Group = styled.div`
   @media (max-width: ${({ theme }) => theme.breakpoint.small}px) {
     flex-direction: column;
     padding: 20px;
+  }
+`;
+
+export const Collections = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 40px;
+  width: 100%;
+`;
+
+export const Collection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+  border-bottom: 2px solid ${({ theme }) => theme.color.background.tertiary};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}px) {
+    flex-direction: column;
+  }
+
+  &:hover {
+    border-bottom: 2px solid ${({ theme }) => theme.color.accent.primary};
+  }
+`;
+
+export const Fields = styled.p`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const Field = styled.span`
+  font-size: 1.1rem;
+  gap: 20px;
+`;
+
+export const FieldTitle = styled.span`
+  color: ${({ theme }) => theme.color.accent.primary};
+`;
+
+export const Settings = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  gap: 50px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}px) {
+    flex-direction: unset;
+    margin-bottom: 20px;
+  }
+`;
+
+export const Icon = styled.p`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.color.actions.negative};
+  transition: filter 0.3s;
+
+  ${({ settings }) =>
+    settings &&
+    css`
+      color: ${({ theme }) => theme.color.actions.neutral};
+    `}
+
+  &:hover {
+    filter: brightness(110%);
+    cursor: pointer;
+  }
+
+  &:active {
+    filter: brightness(120%);
   }
 `;
