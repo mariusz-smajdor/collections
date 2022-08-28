0aa444f36d5fa26a138bae45ffae8f95f0890fd2
Mariusz <smajdormariusz98@gmail.com>
2022-08-28 16:29:45 +0200

    Styles and data formatting for Items component

--- client/src/pages/Items/index.jsx
+++ client/src/pages/Items/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
@@ -5,22 +6,31 @@ import Container from '../../components/layout/Container';
 import Table from './Table';
 import { useGetCollections } from '../../shared/hooks/useGetCollections';
 import { Title } from '../../assets/UI/textFormatEls';
+import { Group, Subject } from './styled';
+import ItemForm from './ItemForm';
 
 function Items() {
+  const [collection, setCollection] = useState(null);
+  const [itemFields, setItemFields] = useState([]);
   const { id } = useParams();
   const { collections } = useSelector(state => state.collections);
   useGetCollections();
 
-  const collection = collections?.data.find(col => col.name === id);
+  useEffect(() => {
+    setCollection(collections?.data.find(col => col.name === id));
+    if (collection) setItemFields(JSON.parse(collection.items));
+  }, [collection, id, collections]);
 
   return (
     <Container big>
-      <div>
-        <Title>{collection?.name}</Title>
-        <p>Topic: {collection?.topic}</p>
-        <p>Description: {collection?.description}</p>
-      </div>
-      <Table items={collection?.items} />
+      <Title>Collection</Title>
+      <Group>
+        <Subject>Name: {collection?.name}</Subject>
+        <Subject>Topic: {collection?.topic}</Subject>
+        <Subject>Description: {collection?.description}</Subject>
+      </Group>
+      <Table itemFields={itemFields} />
+      <ItemForm itemFields={itemFields} />
     </Container>
   );
 }
